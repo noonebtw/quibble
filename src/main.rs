357d27945495da3d0b7b6348eba8293cbc547332
Janis <janis@nirgendwo.xyz>
2023-07-14 02:41:21 +0200

    export symbols from rust crate

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,12 @@
 
 extern crate alloc;
 
+pub use quibble_rs::{
+    add_10,
+    allocator::{efi_free, efi_malloc},
+    say_hello,
+};
+
 #[lang = "eh_personality"]
 #[no_mangle]
 pub extern "C" fn rust_eh_personality() {}
@@ -23,5 +29,6 @@ extern "efiapi" fn __main(image: Handle, mut st: SystemTable<Boot>) -> Status {
         st.boot_services().set_image_handle(image);
     }
     uefi_services::init(&mut st).unwrap();
+    log::info!("this is add_10: {}", quibble_rs::add_10(3));
     unsafe { efi_main(image, st) }
 }
